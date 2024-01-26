559fe351915d1302ee72d5062c9dc9af397d3002
t.simeongordon <t.simeongordon@gmail.com>
2024-01-26 23:16:59 +0000

    v0: nav bar added

--- ui/src/routes/Nav/index.tsx
+++ ui/src/routes/Nav/index.tsx
@@ -1,8 +1,14 @@
+import './navbar.css';
+
 const Nav =()=> {
     return(
-        <>
-        Nav
-        </>
+        <div className="navbar">
+        <div className="title">Caribbean Restaurant</div>
+        <div className="tabs">
+          <div className="tab">Menu</div>
+          <div className="tab">Location</div>
+        </div>
+      </div>
     )
 }
 
