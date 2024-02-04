e8fdcad139228b5c179f95be0db83033b33ed6c9
t.simeongordon <t.simeongordon@gmail.com>
2024-02-04 02:50:09 +0000

    v0: hero updated

--- ui/src/components/hero/index.tsx
+++ ui/src/components/hero/index.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <img
-        src="https://media.istockphoto.com/id/123493447/photo/lei-on-pineapple-at-the-beach.jpg?s=1024x1024&w=is&k=20&c=B9pakUCFh-YMLF7hcb31cd6PeBKj_iBwW9zhL2M1EdQ="
+        src="https://media.istockphoto.com/id/1166171010/photo/spicy-grilled-jerk-chicken-on-a-plate.jpg?s=1024x1024&w=is&k=20&c=7m1kkPf2w2favA-MrwLrHAQ1JSzI0v9fW6ZfPpG-ReA="
         alt="Hero"
         className="hero-image"
       />
