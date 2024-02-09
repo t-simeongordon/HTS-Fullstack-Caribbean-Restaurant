3c125c333e3cdd9401deda8ca23b7dd78b34a4d6
t.simeongordon <t.simeongordon@gmail.com>
2024-02-09 02:04:18 +0000

    v0: inventoryService created

---
+++ api/inventoryService/src/main/java/com/htc/caribbeanrestaurant/CaribbeanRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.htc.caribbeanrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaribbeanRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaribbeanRestaurantApplication.class, args);
+	}
+
+}
---
+++ api/inventoryService/src/test/java/com/htc/caribbeanrestaurant/CaribbeanRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htc.caribbeanrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaribbeanRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
