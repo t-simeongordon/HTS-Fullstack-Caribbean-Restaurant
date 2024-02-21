55fab30d65ec91bfa1f7b75f27c8609f06cc95f9
t.simeongordon <t.simeongordon@gmail.com>
2024-02-21 08:45:21 +0000

    v0: ui test changed

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from './components/footer';
 import Header from './components/header';
 
