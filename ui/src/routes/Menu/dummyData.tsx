a2b5f2ef2b8eef848e6de6a3d1bfc7bef2332982
t.simeongordon <t.simeongordon@gmail.com>
2024-02-24 06:30:52 +0000

    v0: local data modeled

--- ui/src/routes/Menu/dummyData.tsx
+++ ui/src/routes/Menu/dummyData.tsx
@@ -1,13 +1,13 @@
 // dummyData.js
 export const foodData = [
-    { id: 1, name: "Dish 1" },
-    { id: 2, name: "Dish 2" },
-    { id: 3, name: "Dish 3" },
-    { id: 4, name: "Dish 4" },
-    { id: 5, name: "Dish 5" },
-    { id: 6, name: "Dish 6" },
-    { id: 7, name: "Dish 7" },
-    { id: 8, name: "Dish 8" },
-    { id: 9, name: "Dish 9" },
+    { id: 1, name: "Basmati rice", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 2, name: "Dish 2", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192},
+    { id: 3, name: "Dish 3", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 4, name: "Dish 4", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 5, name: "Dish 5", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 6, name: "Dish 6", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 7, name: "Dish 7", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 8, name: "Dish 8", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
+    { id: 9, name: "Dish 9", inStock: true, price: 5.00, description: "comes with vegetable sauce", starRating: 4.9, allergens:[], kcal: 192 },
   ];
   
