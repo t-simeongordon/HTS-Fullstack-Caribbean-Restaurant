aeddc4cbd81c7702ac5a283d04b9c3d76ef72aee
t.simeongordon <t.simeongordon@gmail.com>
2024-02-25 04:58:03 +0000

    v0: basic constructor added for data stack

---
+++ api/menuService/src/infrastructure/dataStack.ts
@@ -0,0 +1,8 @@
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+
+export default class DataStack extends Stack {
+    constructor(scope: Construct, id: string, props?:StackProps){
+        super(scope, id, props)
+    }
+}
