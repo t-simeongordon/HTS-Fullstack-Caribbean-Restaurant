8c61552c924fbe3501816be1bd7d87bea25fadb9
t.simeongordon <t.simeongordon@gmail.com>
2024-02-29 00:20:51 +0000

    v0: ui: images added to dummy data

---
+++ ui/src/routes/Menu/MenuDisplay/MenuList/Card/index.tsx
@@ -0,0 +1,9 @@
+const Card =()=>{
+    return(
+        <div className="card">
+            <img src=
+        </div>
+    )
+}
+
+export default Card
--- ui/src/routes/Menu/MenuDisplay/MenuList/index.tsx
+++ ui/src/routes/Menu/MenuDisplay/MenuList/index.tsx
@@ -2,22 +2,7 @@ import { MenuItems } from "../../../../model/menu/MenuList"
 import "./MenuList.css"
 const MenuList =({ menuItems }: MenuItems)=>{
     return(
-        <ul className="cards">
-            {menuItems.map((item) => (
-
-          <li className="cards__item" key={item.id}>
-      
-            <div className="card">   
-            IMG   
-                <div className="card__content">
-                    <div className="card__title">{item.name}</div>
-                    
-                </div>
-            </div>
-          </li>
-          // Customize based on your data structure
-        ))}
-        </ul>
+       <
     )
 }
 
